5760f0dce33594512b7748a6c1dd067f5d726f4b
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-09-11 10:27:22 +0300

    Update file.js

--- JS/file.js
+++ JS/file.js
@@ -1056,17 +1056,125 @@
 //   values.push(apartment[key])
 // }
 
-function countProps(object) {
-  let propCount = 0;
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+// for (const key in object) {
+//   if (object.hasOwnProperty(key)) {
+//     propCount = Object.keys(object).length
+//   }
+// }
+//   // Change code above this line
+//   return propCount;
+// }
+
+
+// console.log(countProps({ name: "Mango", age: 2 }))
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// console.log(keys);
+// for (const key of keys) {
+//   values.push(apartment[key])
+// }
+// console.log(values);
+
+// function countProps(object) {
+  
+// let propCount = 0;
+//   const keys = Object.keys(object)
+
+//   for (const key of keys) {
+//       propCount += 1;
+//       }
+
+//   return propCount;
+
+// }
+// console.log({ mail: 'poly@mail.com', isOnline: true, score: 500 })
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const values = Object.values(salaries);
+//   for (const value of values) {
+//     totalSalary += value;
+//   }
+//   // Change code above this line
+//   return totalSalary;
+// }
+// console.log({ mango: 100, poly: 150, alfred: 80 });
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+// console.log(hexColors)
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     // console.log(product.name)
+//     // console.log(product.price)
+
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+//       return null
+// }
+  
+//   // Change code above this line
+
+// console.log(getProductPrice("Grip"));
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//     let value = [];
+//   for (const product of products) {
+//     if (product.hasOwnProperty(propName)) {
+//     value.push(product[propName])
+//     }
+//   }
+//   return value;
+//   // Change code above this line
+// }
+// console.log(getAllPropValues("name"));
+
+function calculateTotalPrice(productName) {
   // Change code below this line
-for (const key in object) {
-  if (object.hasOwnProperty(key)) {
-    propCount = Object.keys(object).length
+  let value = 0;
+
+  for (const product of products) {
+    if (product.hasOwnProperty(productName)) {
+      console.log(product)
+    }
   }
-}
+  // return value;
+
   // Change code above this line
-  return propCount;
 }
-
-
-console.log(countProps({ name: "Mango", age: 2 }))
+console.log(calculateTotalPrice("Radar"))
