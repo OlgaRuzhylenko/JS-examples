cbdc38e293968b28cdfedbc7ebaeec18e4d064d6
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-09-19 06:00:56 +0300

    Update file.js

--- JS/file.js
+++ JS/file.js
@@ -2228,3 +2228,102 @@
 //   // Change code above this line
 // }
 // console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]))
+
+// const numbers = [2, 5, 4, 8, 6, 11, 54]
+// const result = numbers.reduce((acc, item, idx, arr) => {
+//   console.log("acc",acc);
+//   console.log('item', item);
+//   console.log('-------------------------');
+//   return acc + item
+// }, 0 )
+
+// const numbers = [2, 5, 4, 8, 6, 11, 54];
+// const result = numbers.reduce((acc, item) => {
+//   console.log("acc",acc);
+//  console.log('item', item);
+//  acc.push(item * 2)
+//  return acc
+// }, [])
+
+// const players = [
+//   { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//   { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//   { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//   { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce((acc, player) => {
+//   return acc += (player.playtime / player.gamesPlayed);
+//   }, 0);
+
+// [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female"
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male"
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female"
+//   }
+// ]
+
+// //Доповни функцію calculateTotalBalance(users) таким чином, щоб вона рахувала і повертала суму всіх коштів (властивість balance), які зберігають користувачі з масиву users.
+// const calculateTotalBalance = users => users.reduce ((acc, user) => {
+//   return acc + user.balance
+// }, 0)
