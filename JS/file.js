114d17bb5789e9e02d1848c086e93b311a3133ba
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-09-28 18:01:17 +0300

    Update file.js

--- JS/file.js
+++ JS/file.js
@@ -2803,3 +2803,18 @@ const cars = [
 // console.table(storage.items);
 
 3.
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+
+  return callback(pizzaName);
+}
+console.log(makeMessage("Royal Grand", makePizza));
+ console.log(makeMessage("Ultracheese", deliverPizza));
