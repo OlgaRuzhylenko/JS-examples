59c5049a95305ce00390eb52e2e287086d2041e3
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2023-10-19 16:20:48 +0300

    Update 6module.js

--- JS/6module.js
+++ JS/6module.js
@@ -482,3 +482,29 @@
 //     refs.list.innerHTML = listItemsMarcup;
 // }
 
+//========================================
+//потрібно створити функцію, яка буде формуати чергу в магазині
+// 1 параметр це масив, к-ть елементів це к-ть покупців, значення елементу масиву це час, який покупець витратить на касі
+// 2 параметр це кількість кас
+// Повертає об'єкт, це ключ це номер каси, значення - це час черги на касі
+// 12 + 2 + 15 = 29
+// 3 + 5 + 6 + 7 = 21
+// { 1 : 29, 2 : 21 }
+
+// function fn(arr, cash) {
+//     const obj = {};
+
+//     for(let i = 1; i <= cash; i += 1) {
+//         obj[i] = 0;
+//     }
+// // console.log(obj);
+//     arr.forEach(buyer => {
+//         const values = Object.values(obj);
+//         const minValue = Math.min(...values);
+//         const currentCase = values.indexOf(minValue) + 1;
+//      obj[currentCase] += buyer
+//     })
+//     return obj
+// }
+
+// console.log(fn([12, 3, 5, 6, 2, 15, 7], 2));
